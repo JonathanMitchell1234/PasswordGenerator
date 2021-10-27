a8a04946d0610d37ac92d9b1e0a384d3ac601070
Jonathan Mitchell <jonmitchell1234@gmail.com>
2021-10-27 15:41:21 -0400

    added generate password function

--- app.js
+++ app.js
@@ -101,3 +101,41 @@ generatePassword(true, true, true, true, 10)
 
 
 // COMMIT AFTER COMPLETING FUNCTION
+ // The filter method creates a new array with all the items that "pass the test" implemented by the provided function (AKA All the items that cause the function to return a boolean value of true when the function is run using the item as an argument for the item parameter in this example)
+  // Checking if the value for index of 1 in each item in the array is true or false. Also, removing the item from the array if it is false.
+  typesArr = typesArr.filter(item => {
+    console.log(item[1]);
+    return item[1];
+  });
+  console.log(`typesArr:`, typesArr);
+
+  // 3. LOOP OVER THE LENGTH AND CALL THE GENERATOR FUNCTION FOR EACH CHECKED OPTION
+  // Building password with a for loop
+  // NOTE: The value for "length" is the value selected for the length number input
+  for (i = 0; i < length; i += typesCount){
+    // 
+    typesArr.forEach(type => {
+      const funcName = type[0];
+      console.log(funcName);
+      // 
+      generatedPassword += randomFunctions[`funcname`]();
+    });
+  }
+
+
+
+  // 4. ADD THE GENERATED PASSWORD TO THE FINAL VARIABLE AND RETURN IT FROM THE FUNCTION
+
+  const finalPassword = generatedPassword.slice(0, length);
+
+  return finalPassword;
+
+
+
+// Example of generatePassword function
+// NOTE: Using the starting value for when the page first loads
+generatePassword(true, true, true, true, 10);
+
+
+
+// COMMIT AFTER COMPLETING FUNCTION
